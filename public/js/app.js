a244421bf372076a376b3c6b457581d2a0e80eca
Matheus Luchiari <mm.luchiari2002@outlook.com>
2021-06-15 20:40:05 -0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,20 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then( (data) => {
-        console.log( data )
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then( (data) => {
+//         console.log( data )
+//     })
+// })
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-    response.json().then( (data) => {
-        if( data.error ) {
+// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+//     response.json().then( (data) => {
+//         if( data.error ) {
 
-            console.log( data.error )
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+//             console.log( data.error )
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -28,7 +28,7 @@ weatherForm.addEventListener( 'submit', (e) => {
 
     console.log( location )
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then( (data) => {
             if( data.error ) {
                 message_one.textContent = data.error
